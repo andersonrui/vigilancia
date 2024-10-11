ac4da904e9aa4da9ef913011e7e736a50cd7f989
Anderson Rui do Amaral <andersonrui@andersonrui.com.br>
2024-10-11 15:53:14 -0300

    Vinculação ao livewire, e início da criação dos componentes

---
+++ app/resources/views/livewire/secretaries/form.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Formulário
+</div>
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Secretaries;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +12,7 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('secretaries', Secretaries::class)->name('secretaries');
 
 require __DIR__.'/auth.php';
